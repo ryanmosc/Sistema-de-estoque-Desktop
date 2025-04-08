b16072301b5cdbc421f3bcfa5a019090a15e5166
Ryan Moscardini <ryanoliveiramosc.com.098@gmail.com>
2025-04-08 16:53:55 -0300

    Add files via upload

--- estoque/db/conexao.py
+++ estoque/db/conexao.py
@@ -3,15 +3,14 @@ import psycopg2
 def conectar():
     try:
         conexao = psycopg2.connect(
-            dbname="estoque_db",
+            dbname="sistema_db",
             user="postgres",           
             password="q1w2e3", 
             host="localhost",
             port="5432"
         )
         print("[OK] Conexão estabelecida com sucesso!")
-        conexao.close()
+        return conexao
     except Exception as e:
         print(f"[ERRO] Falha na conexão: {e}")
-
-conectar()
+        return None
